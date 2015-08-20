d2705eda1d7aae0491d8be5b6b56cb5f72bb9d1b
laci200270 <laci200270@gmail.com>
2015-08-20 12:29:53 +0200

    Prevent crash on dedicated servers

--- src/main/java/hu/laci200270/energymod/handler/BakeEventHandler.java
+++ src/main/java/hu/laci200270/energymod/handler/BakeEventHandler.java
@@ -3,14 +3,16 @@ package hu.laci200270.energymod.handler;
 import hu.laci200270.energymod.models.ModelPipe;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * @author laci200270
  * @date 2015.08.18.
  */
 public class BakeEventHandler {
+    @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public void bakeEvent(ModelBakeEvent event){
         event.modelRegistry.putObject(new ModelResourceLocation("energymod:eConduit","normal"),new ModelPipe(null));
