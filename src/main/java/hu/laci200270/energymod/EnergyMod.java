dd75ed79be6a2a2a0f9a8d058d07ac74f807df59
laci200270 <laci200270@gmail.com>
2015-10-09 23:48:27 +0200

    Rendering

--- src/main/java/hu/laci200270/energymod/EnergyMod.java
+++ src/main/java/hu/laci200270/energymod/EnergyMod.java
@@ -1,13 +1,13 @@
 package hu.laci200270.energymod;
 
 import hu.laci200270.energymod.blocks.EnergyConduit;
+import hu.laci200270.energymod.handler.BakeEventHandler;
 import hu.laci200270.energymod.tile.TileEnergyConduit;
-import net.minecraftforge.client.model.b3d.B3DLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.Logger;
 
 /**
@@ -32,12 +32,12 @@ public class EnergyMod {
         }
         GameRegistry.registerBlock(conduitEnergy, "eConduit");
         GameRegistry.registerTileEntity(TileEnergyConduit.class, "teConduit");
-        //MinecraftForge.EVENT_BUS.register(new BakeEventHandler());
-        if (event.getSide() == Side.CLIENT) {
+        MinecraftForge.EVENT_BUS.register(new BakeEventHandler());
+        /*if (event.getSide() == Side.CLIENT) {
             B3DLoader.instance.addDomain("energymod");
 
-        }
+        }*/
 
     }
-    
+
 }
