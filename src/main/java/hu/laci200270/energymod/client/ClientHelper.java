72deb0eb6403f7e562536e4a73962d6c03bb5faf
laci200270 <laci200270@gmail.com>
2015-12-28 20:12:29 +0100

    Trying to make circle rotating towards the player. Thanks for @shadekiller666 for the code

---
+++ src/main/java/hu/laci200270/energymod/client/ClientHelper.java
@@ -0,0 +1,43 @@
+package hu.laci200270.energymod.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.client.model.TRSRTransformation;
+import org.lwjgl.opengl.GL11;
+
+import javax.vecmath.AxisAngle4d;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3d;
+
+/**
+ * Created by Laci on 2015. 12. 28..
+ */
+public class ClientHelper {
+    public void setRotation(TileEntity tileEntity){
+        Vector3d teLoc = new Vector3d(tileEntity.getPos().getX(), tileEntity.getPos().getY(), tileEntity.getPos().getZ());
+        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        Vector3d playerLoc = new Vector3d();
+        playerLoc.setX(player.posX);
+        playerLoc.setY(player.posY + player.getEyeHeight());
+        playerLoc.setZ(player.posZ);
+        Vector3d lookVec = new Vector3d(playerLoc.getX() - teLoc.getX(), playerLoc.getY() - teLoc.getY(), playerLoc.getZ() - teLoc.getZ());
+        double angleYaw = Math.atan2(lookVec.getZ(), lookVec.getX()) - Math.PI/2d;
+        double anglePitch = Math.atan2(lookVec.getY(), Math.sqrt(lookVec.getX() * lookVec.getX() + lookVec.getZ() * lookVec.getZ()));
+        AxisAngle4d yaw = new AxisAngle4d(0, 1, 0, -angleYaw);
+        AxisAngle4d pitch = new AxisAngle4d(1, 0, 0, -anglePitch);
+        Quat4f rot = new Quat4f(0, 0, 0, 1);
+        Quat4f yawQuat = new Quat4f();
+        Quat4f pitchQuat = new Quat4f();
+        yawQuat.set(yaw);
+        rot.mul(yawQuat);
+        pitchQuat.set(pitch);
+        rot.mul(pitchQuat);
+        Matrix4f matrix = new Matrix4f();
+        matrix.setIdentity();
+        matrix.setRotation(rot);
+
+        GL11.glRotated(angleYaw, 0, 1, 0);
+    }
+}
--- src/main/java/hu/laci200270/energymod/client/tesrs/TesrEnergyCell.java
+++ src/main/java/hu/laci200270/energymod/client/tesrs/TesrEnergyCell.java
@@ -1,13 +1,20 @@
 package hu.laci200270.energymod.client.tesrs;
 
+import com.sun.javafx.geom.Quat4f;
+import javafx.scene.effect.Light;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Matrix4f;
+
+import javax.vecmath.AxisAngle4d;
+import javax.vecmath.Vector3d;
 
 
 /**
@@ -20,13 +27,17 @@ public class TesrEnergyCell extends TileEntitySpecialRenderer {
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks, int destroyStage) {
 
 
-        GL11.glPushMatrix();
+
+
+
+
+
 
         Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("miencraft:diamond_block"));
         Tessellator.getInstance().getWorldRenderer().startDrawing(GL11.GL_TRIANGLE_FAN);
         GL11.glTranslated(x+0.5F, y + 2F, z+0.5F);
         Tessellator.getInstance().getWorldRenderer().setColorRGBA(255, 255, 0, 128);
-         GL11.glRotatef(-Minecraft.getMinecraft().getRenderManager().playerViewY,0,1F,0);
+
         int NUM_PIZZA_SLICES = 200;
         for (int i = 0; i <= NUM_PIZZA_SLICES; i++) { //NUM_PIZZA_SLICES decides how round the circle looks.
             double SUBDIVISIONS = 180;
@@ -39,5 +50,6 @@ public class TesrEnergyCell extends TileEntitySpecialRenderer {
 
         GL11.glPopMatrix();
     }
+
 
 }
