2ccc678a7f9530f13f3328e2ac70ed00ee4eafff
laci200270 <laci200270@gmail.com>
2015-12-28 17:59:25 +0100

    Trying to rotate cycle towards to player

--- src/main/java/hu/laci200270/energymod/client/tesrs/TesrEnergyCell.java
+++ src/main/java/hu/laci200270/energymod/client/tesrs/TesrEnergyCell.java
@@ -16,32 +16,28 @@ import org.lwjgl.opengl.GL11;
 public class TesrEnergyCell extends TileEntitySpecialRenderer {
 
 
-
-
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks, int destroyStage) {
 
 
-
-
-GL11.glPushMatrix();
+        GL11.glPushMatrix();
 
         Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("miencraft:diamond_block"));
-       Tessellator.getInstance().getWorldRenderer().startDrawing(GL11.GL_TRIANGLE_FAN);
-        GL11.glTranslated(x, y + 1F, z);
-        Tessellator.getInstance().getWorldRenderer().setColorRGBA(255,255,0,128);
-       // GL11.glRotatef(Minecraft.getMinecraft().thePlayer.cameraPitch,0,1F,0);
-        int NUM_PIZZA_SLICES=200;
-        for(int i = 0; i <= NUM_PIZZA_SLICES; i++){ //NUM_PIZZA_SLICES decides how round the circle looks.
-            double SUBDIVISIONS=180;
+        Tessellator.getInstance().getWorldRenderer().startDrawing(GL11.GL_TRIANGLE_FAN);
+        GL11.glTranslated(x+0.5F, y + 2F, z+0.5F);
+        Tessellator.getInstance().getWorldRenderer().setColorRGBA(255, 255, 0, 128);
+         GL11.glRotatef(-Minecraft.getMinecraft().getRenderManager().playerViewY,0,1F,0);
+        int NUM_PIZZA_SLICES = 200;
+        for (int i = 0; i <= NUM_PIZZA_SLICES; i++) { //NUM_PIZZA_SLICES decides how round the circle looks.
+            double SUBDIVISIONS = 180;
             double angle = Math.PI * 2 * i / SUBDIVISIONS;
             //GL11.glVertex2f((float) Math.cos(angle), (float) Math.sin(angle));
-            Tessellator.getInstance().getWorldRenderer().addVertex((float) Math.cos(angle),(float) Math.sin(angle),0);
+            Tessellator.getInstance().getWorldRenderer().addVertex((float) Math.cos(angle), (float) Math.sin(angle), 0);
         }
 
         Tessellator.getInstance().draw();
 
-GL11.glPopMatrix();
+        GL11.glPopMatrix();
     }
 
 }
